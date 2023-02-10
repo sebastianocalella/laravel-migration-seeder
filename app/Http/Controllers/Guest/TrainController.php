288f8deeb88b594cec196dae52d513fae239c099
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-10 17:49:43 +0100

    correct controller error

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index(){
-        //$train = Train::all();
-    return view('home'/*, compact('trains')*/);
+        $trains = Train::all();
+    return view('home', compact('trains'));
     }
 }
