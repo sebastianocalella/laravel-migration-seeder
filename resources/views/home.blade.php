cad7ee690816855648c5f4669b6bd49ba13c362d
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-10 17:49:54 +0100

    add style to content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,26 @@
 @extends('layouts.app')
 
-<h2>trains</h2>
+<h1 class="p-5 text-danger">Trains:</h1>
 
-{{-- @forelse ($trains as $train)
-    <div class="col-3">
-        <div class="card bg-dark">
-            <div class="card-body">
-                <h4 class="card-title">{{$train->train_code}}</h4>
-                <p class="card-text">{{$train->departure_station}}</p>
-                <p class="card-text">{{$train->arrival_station}}</p>
-                <a href="{{$loop->index}}" class="btn">info</a>
-            </div>
+<section class="tickets">
+    <div class="container">
+        <div class="row g-4">
+            @forelse ($trains as $train)
+                <div class="col-3">
+                    <div class="card bg-dark">
+                        <div class="card-body">
+                            <h4 class="card-title"><span class="text-danger">train code: </span>{{$train->train_code}}</h4>
+                            <p class="card-text"><span class="text-danger">partenza: </span>{{$train->departure_station}}</p>
+                            <p class="card-text"><span class="text-danger">arrivo: </span>{{$train->arrival_station}}</p>
+                            <a href="{{$loop->index}}" class="btn btn-danger">info</a>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <p>non ci sono treni da mostrare</p>
+            @endforelse
         </div>
     </div>
-@empty
-    <p>non ci sono treni da mostrare</p>
-@endforelse --}}
+</section>
+
+
